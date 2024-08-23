36d93d137e9df4b6afce1ad30e098fda8e937c05
sahil <shlrjsrivastava@gmail.com>
2024-08-23 22:03:07 +0530

    this is the first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,22 @@
+
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './assets/Home.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length, setlength] = useState(8)
+  const [numberAllow, setnumberAllow] = useState(false)
+  const [characterAllow, setcharacterAllow] = useState(false)
+  const [password, setpassword] = useState("")
 
+  const passwordGenerator = () => {
+    
+
+
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
